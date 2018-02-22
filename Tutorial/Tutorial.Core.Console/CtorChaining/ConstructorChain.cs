3899acb69cac5b350ffea7ee807607d3ec84296b
Loganathan Velsamy <loganathan.velsamy@hotmail.com>
2018-02-22 18:38:55 +0530

    test

---
+++ Tutorial/Tutorial.Core.Console/CtorChaining/ConstructorChain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial.Core.Console.CtorChaining
+{
+    class ChainBase
+    {
+
+        public ChainBase()
+        {
+
+        }
+
+    }
+}
