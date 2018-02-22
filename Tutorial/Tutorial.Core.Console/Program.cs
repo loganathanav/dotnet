481e4b6e2ed40cd2d41a6efc1b6219e51cf43338
Loganathan Velsamy <loganathan.velsamy@hotmail.com>
2018-02-22 10:38:46 +0530

    Sample consoles added

---
+++ Tutorial/Tutorial.Core.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial.Core.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
